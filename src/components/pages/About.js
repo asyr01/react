a9c8daee9f6d2c08b20c7d844ddbc39e9d83448e
example <alisayar0107@gmail.com>
2020-12-28 16:08:49 +0300

    We are taking data from https://jsonplaceholder.typicode.com/todos with axios

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,10 +3,21 @@ import React from 'react'
 function About() {
     return (
         <React.Fragment>
-            <h1>About</h1>
-            <p>This is the TodoList app, coded along by Ali Sayar with a react crash course of Brad Traversy.  </p>
+            <h1 style={headerStyle}>About</h1>
+            <p>This is the TodoList app, coded along by <span style={spanStyle}>Ali Sayar</span> with a react crash course of Brad Traversy.  </p>
         </React.Fragment>
     )
+}
+
+const headerStyle = {
+    textAlign: 'center',
+    fontSize: '20px',
+    color: '#f00'
+}
+
+const spanStyle = {
+    color: '#f00',
+    textDecoration: 'italic'
 }
 
 export default About;
