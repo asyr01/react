d31f080dbb187217007deb5591a58dee6f4bf788
example <alisayar0107@gmail.com>
2020-12-28 15:14:39 +0300

    for unique id, uuid installed. addTodo method partially completed.

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -7,6 +7,8 @@ export class AddTodo extends Component {
     
     onSubmit = (e) => {
         e.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({title: ''})
        }
 
     onChange = (e) => {
