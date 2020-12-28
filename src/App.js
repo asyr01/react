bf46f45694fd13ac7b505ce95289da46af7e68da
example <alisayar0107@gmail.com>
2020-12-28 15:31:27 +0300

    we are adding About page with React.Fragment(like a ghost element)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Header from './components/layout/Header'
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 import './App.css';
 
@@ -11,17 +11,17 @@ class App extends Component {
   state = {
     todos: [
       {
-          id: uuid.v4(),
+          id: uuidv4(),
           title: 'Take out the trash',
           completed: false
       },
       {
-        id: uuid.v4(),
+        id: uuidv4(),
         title: 'Dinner with wife',
         completed: false
       },
       {
-        id: uuid.v4(),
+        id: uuidv4(),
         title: 'Meeting with boss',
         completed: false
       }
@@ -46,7 +46,7 @@ delTodo = (id) => {
 // Add Todo
 addTodo = (title) => {
   const newTodo = {
-    id: uuid.v4(),
+    id: uuidv4(),
     title: title,
     completed: false
   }
